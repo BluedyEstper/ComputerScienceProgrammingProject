1dce945090eadbe9aeabcd7b61e6aea799893bb3
BluedyEstper <notsanspun@gmail.com>
2026-04-06 15:33:14 +0100

    V1.0
    Current plan for the code. defining the code will be added next update

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+# Simple Python starter script.
+
+# database 2D array startup
+database = []
+
+# layout of the database will be:
+# [name, job_position, hours worked]
+# job positions and their hourly rates:
+# Front Desk - $14 USD per hour
+# Kitchen - $16 USD per hour
+# House Keeping - $15 USD per hour
+# Maintenance - $20 USD per hour
+# remember workers must work at least 100 hours per month
+
+# code should ask for the names, job positions, and hours worked for 10 workers
+# use a function to have the database ready first
+# remember you have to define a function first before you can call it
+
